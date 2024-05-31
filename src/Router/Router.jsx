73c8e163dd17afdaaf6e21a23e53b3da2284d9d0
DevSujon <sujonmaia274@gmail.com>
2024-06-01 00:52:41 +0600

    sign up and sign in page

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Main/Main";
 import Home from "../Home/Home";
 import Login from "../loginandregister/Login";
+import Signup from "../loginandregister/Signup";
 
 const router = createBrowserRouter([
     {
@@ -15,6 +16,10 @@ const router = createBrowserRouter([
             {
                 path:'/login',
                 element:<Login></Login>
+            },
+            {
+                path:'/signup',
+                element:<Signup></Signup>
             }
         ]
 
--- src/loginandregister/Login.jsx
+++ src/loginandregister/Login.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 
 const Login = () => {
     const [showpassword , setShowpassword] = useState()
     return (
-        <section className="bg-gray-100 min-h-screen flex box-border justify-center items-center">
+        <section className="bg-gray-100 py-10 md:py-20 md:min-h-screen flex box-border justify-center items-center">
             <div className="bg-[#dfa674] rounded-2xl flex max-w-3xl p-5 items-center">
                 <div className="md:w-1/2 px-8">
                     <h2 className="font-bold text-3xl text-[#002D74]">Login</h2>
@@ -13,7 +15,12 @@ const Login = () => {
                     <form action="" className="flex flex-col gap-4">
                         <input className="p-2 mt-8 rounded-xl border" type="email" name="email" placeholder="Email" />
                         <div className="relative">
-                            <input className="p-2 rounded-xl border w-full" type="password" name="password" id="password" placeholder="Password" />
+                            <input className="p-2 rounded-xl border w-full" type={showpassword ? 'text' : 'password'} name="password" id="password" placeholder="Password" />
+                            <div className="absolute right-5 bottom-3" onClick={() => setShowpassword(!showpassword)}>
+                                {
+                                    showpassword ? <FaEyeSlash className="text-xl"></FaEyeSlash> : <FaEye className="text-xl"></FaEye>
+                                }
+                            </div>
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                                 className="bi bi-eye-slash-fill absolute top-1/2 right-3 -z-1 -translate-y-1/2 cursor-pointer hidden"
                                 id="mama" viewBox="0 0 16 16">
@@ -46,11 +53,11 @@ const Login = () => {
 
                     <div className="mt-4 text-sm flex justify-between items-center container-mr">
                         <p className="mr-3 md:mr-0 ">If you don,t have an account..</p>
-                        <button className="hover:border register text-white bg-[#002D74] hover:border-gray-400 rounded-xl py-2 px-5 hover:scale-110 hover:bg-[#002c7424] font-semibold duration-300">Register</button>
+                        <Link to={'/signup'} className="hover:border register text-white bg-[#002D74] hover:border-gray-400 rounded-xl py-2 px-5 hover:scale-110 hover:bg-[#002c7424] font-semibold duration-300">Register</Link>
                     </div>
                 </div>
                 <div className="md:block hidden w-1/2">
-                    <img className="rounded-2xl max-h-[1600px]" src="https://images.unsplash.com/photo-1552010099-5dc86fcfaa38?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHwxfHxmcmVzaHxlbnwwfDF8fHwxNzEyMTU4MDk0fDA&ixlib=rb-4.0.3&q=80&w=1080" alt="login form image" />
+                    <img className="rounded-2xl h-[500px] object-cover" src="https://i.ibb.co/kMRGWpy/vecteezy-intertwined-security-locks-in-abstract-3d-rendering-24061823.jpg" alt="login form image" />
                 </div>
             </div>
         </section>
